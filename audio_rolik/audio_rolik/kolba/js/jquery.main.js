4413d543f1b5140d8c229166048b139265a5d03d
Yauhen Aliakseyenko <yauheni.aliakseyenko@neklo.com>
2015-08-01 13:20:09 +0300

    up

---
+++ audio_rolik/audio_rolik/kolba/js/jquery.main.js
@@ -0,0 +1,49 @@
+var url = 'http://tut.by';
+function getUrlVar(name)
+{
+
+ if (name == 'url')
+ {
+  var i = window.location.href.indexOf(name + '=');
+  if (i >= 0) 
+  {
+   var url = window.location.href.substr(i + name.length + 1);
+   //console.log(url);
+   //console.log(encodeURIComponent(url));
+   //console.log(decodeURIComponent(url));
+   return url;
+  }
+ }
+
+ name = name.replace(/[[]/,"[").replace(/[]]/,"]");
+ var regexS = "[?&]"+name+"=([^&#]*)";
+ var regex = new RegExp( regexS );
+ var results = regex.exec( document.location.href );
+ if(results == null) return "";
+ else return results[1];
+
+}
+
+$(document).ready(function(){
+	$(window).load(function(){
+		$('.slide1').addClass('active');
+		var interval = setInterval(function() {
+			$('section.active').removeClass('active').next('section').addClass('active');
+			if(!$('section.active').next('section').size()) {
+				clearInterval(interval);
+			}
+		}, 3000);
+	});
+	//timer
+	var counter = 20;
+	var interval = setInterval(function() {
+	    counter--;
+	    $('#timerSeconds').html(counter);
+	    if (counter < 10) {
+	    	$('#timerSeconds').html('0'+counter);
+	    }
+	    if (counter < 1) {
+	       document.location.href = unescape(getUrlVar('advertiserUrl'));
+	    }
+	}, 1000);
+});
